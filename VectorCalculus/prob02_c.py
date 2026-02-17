5bbfdc5a4bb26fe525e3a7a8dc50a981b322ad93
MinsooJoo <mjoo3@ncsu.edu>
2026-02-16 23:50:01 -0500

    HW1_VectorCalculus
    Re-upload omitted file

--- VectorCalculus/prob02_c.py
+++ VectorCalculus/prob02_c.py
@@ -11,7 +11,14 @@ def u(x1,x2):
 def dfou(x_1,x_2):
     
     ### <--- START OF YOUR CODE
-    dfou = np.array(0)
+    u_val = u(x_1, x_2)
+    z_1 = u_val[0, 0]
+    z_2 = u_val[1, 0]
+
+    Df_val = Df(z_1, z_2)
+    Du_val = Du(x_1, x_2)
+
+    dfou = Df_val @ Du_val
     ### END OF YOUR CODE --->
 
     return dfou
